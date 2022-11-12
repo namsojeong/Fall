ecd7fb8646b4b7095c67407261eb6fab82b367f8
daehee719 <daeheuigang@gmail.com>
2022-11-13 02:09:18 +0900

    2022-11-13 플레이어 달리기 + 애니메이션 구현중.

--- Assets/01_Scripts/Daehee/PlayerController.cs
+++ Assets/01_Scripts/Daehee/PlayerController.cs
@@ -6,9 +6,14 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody _playerRB;
 
+    private Animator _playerAnim;
     #region PlayerMoveSettingValue
     [SerializeField]
-    private float _speed = 10f;
+    private float _walkSpeed = 10f;
+    [SerializeField]
+    private float _runSpeed = 20f;
+    [SerializeField]
+    private float curSpeed;
     [SerializeField]
     public float _jumpHeight = 10f;
     [SerializeField]
@@ -26,6 +31,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         _playerRB = this.GetComponent<Rigidbody>();
+        _playerAnim = this.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -35,13 +41,19 @@ public class PlayerController : MonoBehaviour
         _moveDir.z = Input.GetAxis("Vertical");
         _moveDir.Normalize();
         CheckGround();
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            curSpeed = _runSpeed;
+        }
+        else
+            curSpeed = _walkSpeed;
         if (Input.GetButtonDown("Jump")&&_isGround)
         {
             _playerRB.drag = 3;
             Vector3 jumpPower = Vector3.up * _jumpHeight;
             _playerRB.AddForce(jumpPower, ForceMode.VelocityChange);
         }
-        if (Input.GetKeyDown(KeyCode.LeftShift))
+        if (Input.GetKeyDown(KeyCode.F))
         {
             Debug.Log("Dash");
             _playerRB.drag = 10;
@@ -55,10 +67,9 @@ public class PlayerController : MonoBehaviour
         {
             if (Mathf.Sign(transform.forward.x) != Mathf.Sign(_moveDir.x) || Mathf.Sign(transform.forward.z) != Mathf.Sign(_moveDir.z))
                 transform.Rotate(0, 1, 0);
-            transform.forward = Vector3.Lerp(transform.forward,_moveDir,Time.deltaTime* _rotateSpeed);
+            transform.forward = Vector3.Lerp(transform.forward, _moveDir, Time.deltaTime * _rotateSpeed);
         }
-
-        _playerRB.MovePosition(this.gameObject.transform.position + _moveDir * _speed * Time.deltaTime);
+        _playerRB.MovePosition(this.gameObject.transform.position + _moveDir * curSpeed * Time.deltaTime);
     }
 
 
