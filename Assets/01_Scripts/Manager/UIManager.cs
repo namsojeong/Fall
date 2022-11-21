303d520a666a43684ac1214761f7622db8d6a3cc
Namsojeong <nsj050320@gmail.com>
2022-11-21 21:19:42 +0900

    22-11-21 Start씬 세팅 (스카이박스, UI 배치)

---
+++ Assets/01_Scripts/Manager/UIManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public void OffUI(GameObject ui)
+    {
+        ui.SetActive(false);
+    }
+
+    public void OnUI(GameObject ui)
+    {
+        ui.SetActive(true);
+    }
+}
