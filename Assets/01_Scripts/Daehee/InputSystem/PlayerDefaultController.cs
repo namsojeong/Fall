438a0247edb8f9fa877059d4d72ecdf6e2d48ea9
daehee719 <daeheuigang@gmail.com>
2022-12-09 11:51:01 +0900

    2022-12-09 일반전 수정

--- Assets/01_Scripts/Daehee/InputSystem/PlayerDefaultController.cs
+++ Assets/01_Scripts/Daehee/InputSystem/PlayerDefaultController.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,7 +11,11 @@ public class PlayerDefaultController : MonoBehaviour
     private InputAction moveAction;
 
     private Vector2 inputVec;
+    private Vector2 playerPos;
+    private float radLook;
+    private float angle;
 
+    public GameObject bulletPrefab;
     void Start()
     {
         controller = GetComponent<CharacterController>();
@@ -21,8 +26,36 @@ public class PlayerDefaultController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        inputVec = moveAction.ReadValue<Vector2>();
-        Vector3 move = new Vector3(inputVec.x, 0, inputVec.y);
+        Vector2 input = moveAction.ReadValue<Vector2>();
+        Vector3 move = new Vector3(input.x, 0, input.y);
+        move.y = 0;
         controller.Move(move.normalized * Time.deltaTime * 10f);
+
+        playerPos = Camera.main.WorldToScreenPoint(transform.position);
+        radLook = Mathf.Atan2(Input.mousePosition.y - playerPos.y, Input.mousePosition.x - playerPos.x);
+        angle = radLook * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, -angle + 120, 0);
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            ShootGunBoss();
+        }
+    }
+    private void ShootGunBoss()
+    {
+        Debug.Log("shoot");
+        RaycastHit hit;
+        GameObject bullet = Instantiate(bulletPrefab, transform.position,Quaternion.identity);
+        BulletController bulletController = bullet.GetComponent<BulletController>();
+        if (Physics.Raycast(transform.position, transform.forward, out hit, Mathf.Infinity))
+        {
+            bulletController.target = hit.point;
+            bulletController.hit = true;
+        }
+        else
+        {
+            bulletController.target = transform.position + transform.forward * 1000;
+            bulletController.hit = false;
+        }
     }
 }
